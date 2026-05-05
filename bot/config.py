e14010befdb2945a8d77261d5e4aaf05029b6961
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2026-05-05 14:09:47 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION_STRING = os.getenv("SESSION_STRING")
+MONGO_URI = os.getenv("MONGO_URI")
+OWNER_ID = int(os.getenv("OWNER_ID"))
