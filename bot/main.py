c1012270efdf7ed627e2112066b2e4a975913161
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2026-05-05 14:37:40 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -9,12 +9,14 @@ from bot.commands import *
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
+
 @client.on(events.NewMessage(pattern="/start"))
 async def start(event):
     if await is_admin(event.sender_id):
         await event.reply("✅ You are admin. Use /help")
     else:
-        await event.reply("🤖 Private bot. Access denied.")
+        await event.reply("🤖 Private bot.")
+
 
 @client.on(events.NewMessage(pattern="/help"))
 async def _(e): await help_command(e)
@@ -31,14 +33,18 @@ async def _(e): await set_source(e)
 @client.on(events.NewMessage(pattern="/status"))
 async def _(e): await status(e)
 
+
 async def main():
     await client.start()
 
     if not await admins_col.find_one({"admin_id": OWNER_ID}):
         await admins_col.insert_one({"admin_id": OWNER_ID})
 
-    print("Bot running...")
+    print("🚀 Bot Started")
+
+    await asyncio.sleep(5)  # startup delay
     await scheduler(client)
+
 
 with client:
     client.loop.run_until_complete(main())
