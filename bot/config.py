0bf27f118394418794cb41f445110753dd194e35
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2026-05-05 15:28:10 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,7 @@
 import os
+log = os.getenv("LOG_CHANNEL")
 
+LOG_CHANNEL = int(log) if log and log.strip() else 0
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION_STRING = os.getenv("SESSION_STRING")
